241520d112c3c5a13886bad166d985b4defc8e45
Ginepp24 <ginepp24@gmail.com>
2022-03-12 01:46:43 -0500

    adding file information

--- js/index.js
+++ js/index.js
@@ -1,27 +1,54 @@
-window.addEventListener('load', function () {
-    console.log('contenido ha cargado');
-    
-    var imagenes = [];
+unction showSecondScreen() {
+    let hide = document.getElementById("first__screen")
+    let show = document.getElementById("second__screen")
+    hide.style.display = "none";
+    show.style.display = "block";
+}
 
-    imagenes[0] = '../img/img2.jpg'
-    imagenes[1] = '../img/img3.jpg'
-    imagenes[2] = '../img/img4.jpg'
+function showFirstScreen() {
+    let hide = document.getElementById("second__screen")
+    let show = document.getElementById("first__screen")
+    hide.style.display = "none"
+    show.style.display = "block"
+}
 
-    var indiceImagenes = 0;
-    var tiempo = 1000;
+const btnBuy = document.getElementsByClassName('buy_button');
+for (let i = 0; i < btnBuy.length; i++) {
+    btnBuy[i].addEventListener('click', showSecondScreen);
+}
 
-    function cambiarImagenes() {
+const btn = document.getElementById('btnValid');
+btn.addEventListener('click', () => {
 
-        document.slider.src = imagenes[indiceImagenes];
+alert('pronto se desbloqueara esta función');
 
-        if (indiceImagenes < 2){
-            indiceImagenes++;
-        } else {
-            indiceImagenes = 0;
-        }
-    }
-
-    setInterval(cambiarImagenes, tiempo);
-    
-    
 });
+
+const btnBack = document.getElementById("btnBack");
+btnBack.addEventListener('click', showFirstScreen)
+
+window.addEventListener('load', function () {
+   console.log('contenido ha cargado');
+    
+  var imagenes = [];
+
+   imagenes[0] = '../img/img2.jpg'
+    imagenes[1] = '../img/img3.jpg'
+   imagenes[2] = '../img/img4.jpg'
+
+  var indiceImagenes = 0;
+   var tiempo = 1000;
+
+   function cambiarImagenes() {
+
+      document.slider.src = imagenes[indiceImagenes];
+
+       if (indiceImagenes < 2){
+         indiceImagenes++;
+      } else {
+            indiceImagenes = 0;
+     }
+  }
+
+setInterval(cambiarImagenes, tiempo);
+    
